72007906d3a2ae2022f2b8dfab81f1525f8d36d4
Alexander Ivanets <sunberry@tuta.io>
2025-12-15 11:55:43 +0100

    Add Day 6 Part 2

--- src/main/java/com/sunberry/aoc/y2025/Day06.java
+++ src/main/java/com/sunberry/aoc/y2025/Day06.java
@@ -3,6 +3,7 @@ package com.sunberry.aoc.y2025;
 import com.sunberry.aoc.Solution;
 import com.sunberry.aoc.utils.InputUtils;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 
@@ -25,9 +26,42 @@ public class Day06 implements Solution {
 
   }
 
+  private String collectNumber(List<String> grid, int index) {
+    StringBuilder number = new StringBuilder();
+    for (int i = 0; i < grid.size(); ++i) {
+      char currentChar = grid.get(i).length() <= index ? ' ' : grid.get(i).charAt(index);
+      number.append(currentChar);
+    }
+    return number.toString();
+  }
+
   @Override
   public Object part2() {
-    return null;
+    List<String> grid = InputUtils.readLines("2025", "day06");
+    int firstIndex = grid.stream().mapToInt(String::length).max().getAsInt() - 1;
+    List<Long> results = new ArrayList<>();
+    List<Integer> numbers = new ArrayList<>();
+    for (int i = firstIndex; i >= 0; --i) {
+      String currentNumber = collectNumber(grid, i);
+      if (currentNumber.isBlank()) {
+        continue;
+      }
+      if (!currentNumber.contains("+") && !currentNumber.contains("*")) {
+        numbers.add(Integer.parseInt(currentNumber.strip()));
+      } else {
+        char operator = currentNumber.charAt(currentNumber.length() - 1);
+        currentNumber = currentNumber.substring(0, currentNumber.length() - 1);
+        numbers.add(Integer.parseInt(currentNumber.strip()));
+        if (operator == '+') {
+          results.add(numbers.stream().mapToLong(x -> x).sum());
+          numbers = new ArrayList<>();
+        } else {
+          results.add(numbers.stream().mapToLong(Integer::longValue).reduce(1, (x, y) -> x * y));
+          numbers = new ArrayList<>();
+        }
+      }
+    }
+    return results.stream().mapToLong(x -> x).sum();
   }
 
   public static void main(String[] args) {
